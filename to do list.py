f4db24f79339e1d48cbcaa2d618b9c10dcf88d20
shakthipriyaa1624 <153530090+shakthipriyaa1624@users.noreply.github.com>
2023-12-28 18:27:54 +0530

    Add files via upload

---
+++ to do list.py
@@ -0,0 +1,51 @@
+class ToDoList:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+        print(f'Task "{task}" added successfully.')
+
+    def remove_task(self, task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+            print(f'Task "{task}" removed successfully.')
+        else:
+            print(f'Task "{task}" not found.')
+
+    def view_tasks(self):
+        if not self.tasks:
+            print('No tasks in the To-Do List.')
+        else:
+            print('To-Do List:')
+            for idx, task in enumerate(self.tasks, start=1):
+                print(f'{idx}. {task}')
+
+def main():
+    todo_list = ToDoList()
+
+    while True:
+        print('\nOptions:')
+        print('1. Add Task')
+        print('2. Remove Task')
+        print('3. View Tasks')
+        print('4. Exit')
+
+        choice = input('Enter your choice (1/2/3/4): ')
+
+        if choice == '1':
+            task = input('Enter the task: ')
+            todo_list.add_task(task)
+        elif choice == '2':
+            task = input('Enter the task to remove: ')
+            todo_list.remove_task(task)
+        elif choice == '3':
+            todo_list.view_tasks()
+        elif choice == '4':
+            print('Exiting the To-Do List application. Goodbye!')
+            break
+        else:
+            print('Invalid choice. Please enter 1, 2, 3, or 4.')
+
+if __name__ == "__main__":
+    main()
